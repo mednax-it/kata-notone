2579b22d8a1bce399b838b2e21b3068f26113ec1
Kyle Adams <kadams54@users.noreply.github.com>
2024-02-13 11:54:24 -0500

    🧑‍💻 Add some players for running tournaments

---
+++ notone/players/p11.py
@@ -0,0 +1,17 @@
+from notone.types import GameState
+
+
+def name() -> str:
+    return "Eleven"
+
+
+def emoji() -> str:
+    return "1️⃣1️⃣"
+
+
+def victory_cry() -> str:
+    return f"{name()} reigns supreme."
+
+
+def roll_again(state: GameState) -> bool:
+    return state.turn_rolls < 4
