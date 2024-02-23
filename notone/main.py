0462de64566d96a57e6d02ef05e2479edaa43de6
Kyle Adams <kadams54@users.noreply.github.com>
2024-02-23 09:14:00 -0500

    💄 Run games/tourneys as subcommands
    This change makes it easier to add other modes of play, like a stress
    test mode that puts each player through 10,000 games and finds their
    average score.

--- notone/main.py
+++ notone/main.py
@@ -8,22 +8,37 @@ https://github.com/mednax-it/kata-notone#creating-your-own-player
 """
 import typer
 
-from notone import console, game, players, tournament
+from notone import (
+    console,
+    game as game_runner,
+    players,
+    tournament as tournament_runner,
+)
+
+app = typer.Typer()
 
 
-def main():
-    try:
-        opponents = players.load()
-        if len(opponents) > 2:
-            console.connect_output("tournament")
-            tournament.play(opponents)
-        else:
-            console.connect_output("game")
-            game.play(opponents)
-    except Exception as e:
-        console.error(e)
-        raise typer.Exit(code=1) from e
+@app.command()
+def game():
+    opponents = players.load()
+    if len(opponents) > 2:
+        console.echo(
+            f"{len(opponents)} players registered; a game can only by played "
+            "by 2 players. Try [primary]`notone tournament`[/] or modify the "
+            "[primary]`notone/players/__init__.py`[/] file to register fewer "
+            "players.",
+            style="error",
+        )
+        raise typer.Exit(1)
+    console.connect_output("game")
+    game_runner.play(opponents)
+
+
+@app.command()
+def tournament():
+    console.connect_output("tournament")
+    tournament_runner.play(players.load())
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
